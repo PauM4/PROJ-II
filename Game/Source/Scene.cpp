24128d8696d3edb45731fb6b3f65b0fa8586f105
Nacho Moreno <nmoreno2000@gmail.com>
2023-04-01 14:57:26 +0200

    Solved error that make the scene crash

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -39,10 +39,14 @@ bool Scene::Awake(pugi::xml_node& config)
 	}
 
 	//L02: DONE 3: Instantiate the player using the entity manager
-	//if (config.child("player")) {
-	//	player = (Player*)app->entityManager->CreateEntity(EntityType::PLAYER);
-	//	player->parameters = config.child("player");
-	//}
+	/*
+	if (config.child("player")) {
+		player = (Player*)app->entityManager->CreateEntity(EntityType::PLAYER);
+		player->parameters = config.child("player");
+	}
+	*/
+	player = (Player*)app->entityManager->CreateEntity(EntityType::PLAYER);
+	player->parameters = config.child("player");
 
 	//L02: DONE 3: Instantiate the player using the entity manager
 	npc1 = (Npc*)app->entityManager->CreateEntity(EntityType::NPC);
