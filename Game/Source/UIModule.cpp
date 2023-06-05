e1dc9448e427cb5075c0df4265c03499a0f4197c
xaita <99950138+xaita@users.noreply.github.com>
2023-06-05 21:42:36 +0200

    arreglos

--- Game/Source/UIModule.cpp
+++ Game/Source/UIModule.cpp
@@ -526,18 +526,18 @@ bool UIModule::PostUpdate()
 	}
 
 	if (app->sceneBattle->active) {
-		// Tutorial Battle draw
-		if (app->scene->battleTutorialCounter == 0 && app->scene->isTalkingToAngry)
+
+		if (app->scene->battleTutorialCounter == 0)
 		{
 			SDL_Rect rect = { 0, 0, 1920, 1080 };
 			app->render->DrawTexture(app->scene->battleTutoTexture, -app->render->camera.x, -app->render->camera.y, &rect);
 		}
-		else if (app->scene->battleTutorialCounter == 1 && app->scene->isTalkingToAngry)
+		else if (app->scene->battleTutorialCounter == 1)
 		{
 			SDL_Rect rect = { 1920, 0, 1920, 1080 };
 			app->render->DrawTexture(app->scene->battleTutoTexture, -app->render->camera.x, -app->render->camera.y, &rect);
 		}
-		else if (app->scene->battleTutorialCounter == 2 && app->scene->isTalkingToAngry)
+		else if (app->scene->battleTutorialCounter == 2)
 		{
 			SDL_Rect rect = { 3840, 0, 1920, 1080 };
 			app->render->DrawTexture(app->scene->battleTutoTexture, -app->render->camera.x, -app->render->camera.y, &rect);
