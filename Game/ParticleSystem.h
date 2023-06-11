fa45053045c0a9342873616fcbf0be68242e99fe
PauM4 <pau.argiz@estudiantat.upc.edu>
2023-06-11 13:19:36 +0200

    ParticleSystem

---
+++ Game/ParticleSystem.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "SDL/include/SDL.h"
+
+#define FADE_DELAY 700;
+
+struct ParticleProps {
+	float x;
+	float y;
+	int velocity_x;
+	int velocity_y;
+	float spreadfactor;
+	float lifetime;
+	float beginscale;
+	int r, g, b;
+	int r2, g2, b2;
+	float scaleVariation;
+	float endscale;
+};
+
+struct Particle {
+	float x;
+	float y;
+	int velocity_x;
+	int velocity_y;
+	float lifetime;
+	float lifetimeremaining;
+	float beginscale;
+	float endscale;
+	int r, g, b;
+	int r2, g2, b2;
+	bool Active;
+};
+
+class ParticleSystem
+{
+public:
+
+	ParticleSystem();
+
+	~ParticleSystem();
+
+	void UpdateParticles(float dt);
+
+	void Emit(ParticleProps& particleProps);
+	void render(SDL_Renderer* renderer);
+	float lerp(float a, float b, float f);
+
+	std::vector<Particle> ParticleList;
+
+private:
+	int currentparticle;
+};
+
